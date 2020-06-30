97fcff62e374c90b3c34b11caa84dbe8ea96b6b3
Anthony <anthony.trillo.92@gmail.com>
2020-06-30 11:32:16 -0300

    Formulario estilizado

--- src/components/LinkForm.js
+++ src/components/LinkForm.js
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const LinkForm = () => {
-  return (
-    <form className="card card-body">
 
+  const initialStateVaues = {
+    url: '',
+    name: '',
+    description: ''
+  }
+
+  const [values, setValues] = useState(initialStateVaues);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(e)
+  }
+
+  return (
+    <form className="card card-body" onSubmit={ handleSubmit }>
+      <div className="form-group input-group">
+        <div className="input-group-text bg-light">
+          <i className="material-icons">insert_link</i>
+        </div>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="https://someurl.com"
+          name="url"
+        />
+      </div>
+
+      <div className="form-group input-group">
+        <div className="input-group-text bg-light">
+          <i className="material-icons">create</i>
+        </div>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Website name"
+          name="name"
+        />
+      </div>
+
+      <div className="form-group">
+        <textarea
+        name="decription"
+        row="3"
+        className="form-control"
+        placeholder="Write a description">
+        </textarea>
+      </div>
+
+      <button className="btn btn-primary btn-block">
+        Save
+      </button>
     </form>
-  )
+  );
 };
 
 export default LinkForm;
